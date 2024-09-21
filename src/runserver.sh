2871b1a9d5ffcd964ab7f17fea1ead7378695008
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-21 14:38:45 +0200

    Changement PATH

---
+++ src/runserver.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"C:\Users\mathi\OneDrive\Documents\BUT2\PYCHARM PROJECT\pythonProject\venv\Scripts\python.exe" "C:\Users\mathi\OneDrive\Documents\BUT2\PYCHARM PROJECT\pythonProject\src\manage.py" runserver

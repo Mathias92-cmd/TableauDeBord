7f04d988bc10e0b176234cd6911880049bde91d3
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-21 10:15:20 +0200

    Update2.0 get_rates

--- api.py
+++ api.py
@@ -4,19 +4,29 @@ from pprint import pprint
 import requests
 
 def get_rates(currencies , days=30):
-    end_date = date.today()
-    start_date = end_date - timedelta(days=days)
+    endDate = date.today()
+    startDate = endDate - timedelta(days=days)
 
-    join = ','.join(currencies)
-    requete = f"https://www.docstring.fr/api/rates/history/?start_at={start_date}&end_at={end_date}&symbols={join}"
+    symbols = ','.join(currencies)
+    requete = f"https://www.docstring.fr/api/rates/history/?start_at={startDate}&end_at={endDate}&symbols={symbols}"
     r = requests.get(requete)
     if not r and not r.json():
         return False, False
 
-    api_rates = r.json().get("rates")
-    pprint(api_rates)
+    apiRates = r.json().get("rates")
+
+    allRates = {currency: [] for currency in currencies}
+    allDays = (sorted(apiRates.keys()))
+
+    for eachDay in allDays:
+        [allRates[currency].append(rate) for currency, rate in apiRates[eachDay].items()]
+    return allDays , allRates
+
+
     return None, None
 
 
 if __name__ == '__main__':
     days , rates = get_rates(currencies=["USD" , "CAD"])
+    pprint(days)
+    pprint(rates)

067d548f64f1f8d42f1ab38322664ef59a810e44
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-22 10:35:20 +0200

    Update view and html

--- src/devise/views.py
+++ src/devise/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render , HttpResponse
 
+from DashboardDevise import api
+
+
 # Create your views here.
 
 def dashboard(request):
-    return render(request, "devise/index.html" , context={"list_nombres": range(10)})
+    days , rates = api.get_rates(currencies=["USD"] , days=30)
+    return render(request, "devise/index.html" , context={"data": rates["USD"] ,
+                                                                        "days_labels": days})

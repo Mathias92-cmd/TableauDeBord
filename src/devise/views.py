0e29398a03347ce787b4b2ec9d44cb62f15b5b29
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-22 09:47:59 +0200

    Mise en place de la page html

--- src/devise/views.py
+++ src/devise/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 
 def dashboard(request):
-    return HttpResponse("<h1>Bonjour tout le monde</h1>")
+    return render(request, "devise/index.html" , context={"list_nombres": range(10)})

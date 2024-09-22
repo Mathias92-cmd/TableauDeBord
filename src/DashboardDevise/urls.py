6da8eca389e9401fbe118883b390139435b4d2ff
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-22 10:59:35 +0200

    Affichage de plusieurs graphes sur une page

--- src/DashboardDevise/urls.py
+++ src/DashboardDevise/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from devise.views import dashboard
 
 urlpatterns = [
-    path("" , dashboard),
+    path("days=<int:days_range>&currencies=<str:currencies>" , dashboard),
     path('admin/', admin.site.urls),
 ]
 
--- src/devise/views.py
+++ src/devise/views.py
@@ -5,7 +5,7 @@ from DashboardDevise import api
 
 # Create your views here.
 
-def dashboard(request):
-    days , rates = api.get_rates(currencies=["USD"] , days=30)
-    return render(request, "devise/index.html" , context={"data": rates["USD"] ,
+def dashboard(request, days_range=60 , currencies="USD"):
+    days , rates = api.get_rates(currencies=currencies.split(",") , days=days_range)
+    return render(request, "devise/index.html" , context={"data": rates ,
                                                                         "days_labels": days})

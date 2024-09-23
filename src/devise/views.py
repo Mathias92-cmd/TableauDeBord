fe43bd77a2e0b779e151b59f60fd2f836d749c94
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-23 18:46:01 +0200

    Ajout php dans la page d'accueil

--- src/devise/views.py
+++ src/devise/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect as django_redirect
 from DashboardDevise import api
 
 def custom_redirect(request):
-    return django_redirect("dashboard_with_params", days_range=7, currencies="USD")
+    return django_redirect("dashboard_with_params", days_range=7, currencies="USD,PHP")
 
 def dashboard(request, days_range=60, currencies="USD"):
     days, rates = api.get_rates(currencies=currencies.split(","), days=days_range)
